0e845691d52638ce766a773e3a2085b9188726d3
hirunikaveesha <88107879+hirunikaveesha@users.noreply.github.com>
2022-06-05 13:15:06 +0530

    Add files via upload

---
+++ ATM Machine.cpp
@@ -0,0 +1,44 @@
+# include <iostream>
+using namespace std;
+
+void showMenu(){
+	cout<<"**********Menu**********"<<endl;
+	cout<<"1.Check Balance"<<endl;
+	cout<<"2.Deposit"<<endl;
+	cout<<"3.Withdraw"<<endl;
+	cout<<"4.Exit"<<endl;
+	cout<<"************************"<<endl;
+}
+
+
+int main(){
+	//check balance,deposit,withdraw,show menu
+	int op;
+	double balance=500;
+	
+	do{
+	showMenu();
+	cout<<"Option:";
+	cin>>op;
+	system("cls");
+	switch(op){
+		case 1:cout<<"Balance is: "<<balance<<endl;break;
+		case 2:cout<<"Deposit Amount:";
+			double depositAmount;
+			cin>>depositAmount;
+			balance+=depositAmount;
+			break;
+		case 3:cout<<"Withdraw Amount:";
+			double withdrawAmount;
+			cin>>withdrawAmount;
+			if(withdrawAmount<= balance)
+				balance-=withdrawAmount;
+			else
+				cout<<"Not enough Money"<<endl;
+			break;
+				}
+}while(op!=4);
+
+return 0;
+	
+}
